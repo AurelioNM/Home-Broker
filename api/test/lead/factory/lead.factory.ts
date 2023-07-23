374a57de30d28d57847621d029c9d4751765c27c
Aurelio_Mororo <aurelio@globalfy.com>
2023-07-23 03:03:39 -0300

    feat: add final tests to LeadController; refactor: change tests factory for leads

--- api/test/lead/factory/lead.factory.ts
+++ api/test/lead/factory/lead.factory.ts
@@ -1,11 +1,41 @@
 import { generateUUID } from '~/common-util/uuid';
 import { CreateLeadDto } from '~/lead/dto/create-lead.dto';
+import { GetLeadDto } from '~/lead/dto/get-lead.dto copy';
 import { LeadEntity } from '~/lead/entities/lead.entity';
 
-export const mockOneCompanyLead = (): LeadEntity => {
+export const mockListGetLeadDto = (): GetLeadDto[] => {
+  const leadList: GetLeadDto[] = [];
+
+  for (let i = 1; i <= 3; i++) {
+    const lead: GetLeadDto = new GetLeadDto();
+
+    lead.id = generateUUID();
+    lead.createddate = new Date(new Date().toUTCString());
+    lead.customerId = null;
+    lead.data = {
+      cpf: `153-995-492-6${i}`,
+      name: `Lead ${i}`,
+      email: `leadtest${i}@gmail.com`,
+      surname: 'Test',
+    };
+
+    leadList.push(lead);
+  }
+
+  return leadList;
+};
+
+export const mockOneLeadEntity = (): LeadEntity => {
   const lead: LeadEntity = new LeadEntity();
   lead.id = generateUUID();
-  lead.data = mockOneCreateLeadDto();
+  lead.createddate = new Date(new Date().toUTCString());
+  lead.customerId = null;
+  lead.data = {
+    cpf: '153-995-492-61',
+    name: 'Agnaldo',
+    email: 'agnaldo@gmail.com',
+    surname: 'Pereira',
+  };
 
   return lead;
 };
@@ -18,24 +48,4 @@ export const mockOneCreateLeadDto = (): CreateLeadDto => {
   createLeadDto.surname = 'Lead';
 
   return createLeadDto;
-};
-
-export const mockListLead = (): LeadEntity[] => {
-  const leadList: LeadEntity[] = [];
-
-  for (let i = 1; i <= 3; i++) {
-    const lead: LeadEntity = new LeadEntity();
-    const createLeadDto: CreateLeadDto = new CreateLeadDto();
-    createLeadDto.cpf = `434-583-434-6${i}`;
-    createLeadDto.email = `fakelead${i}@gmail.com`;
-    createLeadDto.name = `Fake`;
-    createLeadDto.surname = `Lead ${i}`;
-
-    lead.id = generateUUID();
-    lead.data = createLeadDto;
-
-    leadList.push(lead);
-  }
-
-  return leadList;
 };
